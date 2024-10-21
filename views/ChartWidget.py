864dea3c6bdbc340d420b66cdee883df28f7f14b
Alexey Maslennikov <leha.maslennikov2003@gmail.com>
2024-10-21 11:36:12 +0500

    ChartWidget: draw_chart_wrapper - decorator with scale, shift and gen(min_x, min_y, max_x, max_y)

--- views/ChartWidget.py
+++ views/ChartWidget.py
@@ -7,15 +7,14 @@ class ChartWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.center_ratio = (0.3, 0.8)
+        self.shift = QtCore.QPoint(0, 0)
         self.scale = 10
-        self.draw_callback: list[Callable[[ChartWidget, QtGui.QPainter], None]] = [
-            axis
-        ]
+        self.draw_callback: list[Callable[[ChartWidget, QtGui.QPainter], None]] = []
 
     def resizeEvent(self, e):
         self.shift = QtCore.QPoint(
             self.size().width() * self.center_ratio[0],
-            -self.size().height() * self.center_ratio[1],
+            self.size().height() * self.center_ratio[1],
         )
 
     def paintEvent(self, event: QtGui.QPaintEvent):
@@ -23,52 +22,29 @@ class ChartWidget(QtWidgets.QWidget):
         painter.begin(self)
         for callback in self.draw_callback:
             callback(self, painter)
-            print(callback)
         painter.end()
 
     def setScale(self, scale: float) -> None:
         self.scale = scale
 
+    def draw_chart_wrapper(
+        self, gen: Callable[[float, float, float, float], Iterator[tuple[float, float]]]
+    ) -> None:
+        def draw_chart(window: ChartWidget, painter: QtGui.QPainter) -> None:
+            path = QtGui.QPainterPath()
+            size = self.size()
+            points = gen(
+                -self.shift.x(),
+                self.shift.y(),
+                size.width() - self.shift.x(),
+                self.shift.y() - size.height(),
+            )
+            x0, y0 = next(points)
+            for x, y in points:
+                x, y = window.scale * x, window.scale * y
+                path.moveTo(window.shift.x() + x0, window.shift.y() - y0)
+                path.lineTo(window.shift.x() + x, window.shift.y() - y)
+                x0, y0 = x, y
+            painter.drawPath(path)
 
-def draw_coords(window: ChartWidget, painter: QtGui.QPainter) -> None:
-    path = QtGui.QPainterPath()
-    path.moveTo(window.shift.x(), 0)
-    path.lineTo(window.shift.x(), window.size().height())
-    path.moveTo(0, window.shift.y())
-    path.lineTo(window.size().width(), window.shift.y())
-    painter.drawPath(path)
-
-
-def draw_chart_wrapper(
-    gen: Callable[[],Iterator[tuple[float, float]]]
-) -> None:
-    def draw_chart(window: ChartWidget, painter: QtGui.QPainter) -> None:
-        path = QtGui.QPainterPath()
-        points = gen()
-        x0, y0 = next(points)
-        for x, y in points:
-            x, y = window.scale * x, window.scale * y
-            path.moveTo(window.shift.x() + x0, window.shift.y() + y0)
-            path.lineTo(window.shift.x() + x, window.shift.y() + y)
-            x0, y0 = x, y
-        painter.drawPath(path)
-    return draw_chart
-
-@draw_chart_wrapper
-def axis():
-    for x in range(-100, 100):
-        yield (x, 0)
-
-
-def _ordinat():
-    for y in range(-1000, 1000):
-        yield (0, y)
-        
-ordinat = draw_chart_wrapper(_ordinat())
-
-
-def xx():
-    for x in range(-100, 100):
-        yield (x, x * x)
-
-draw_xx = draw_chart_wrapper(xx())
+        self.draw_callback.append(draw_chart)
